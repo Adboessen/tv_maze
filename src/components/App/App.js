42582c995e6d159252d846be6a82b184da2b5afb
Andrew B <andrewboessen@amdg.rockhursths.edu>
2022-01-07 12:51:06 -0600

    Step 5

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,25 @@
+import React from 'react';
 import './App.css';
 import Intro from '../Intro/Intro.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <h1 className="App-title">TV Series List</h1>
-      </header>
-      <Intro message="Here you can find all of your most loved series." />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      series: []
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+            <h1 className="App-title">TV Series List</h1>
+        </header>
+        <Intro message="Here you can find all of your most loved series." />
+        The length of series array - {this.state.series.length}
+      </div>
+    );
+  }
 }
 
 export default App;
