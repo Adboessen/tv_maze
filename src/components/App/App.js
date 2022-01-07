2c741e06a8afe879cd63a1365c75b236eacc7dcb
Andrew B <andrewboessen@amdg.rockhursths.edu>
2022-01-07 13:07:19 -0600

    Step 7

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,20 +1,8 @@
 import React from 'react';
 import './App.css';
 import Intro from '../Intro/Intro.js'
-
+import Series from '../../containers/Series/Series.js';
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      series: []
-    }
-  }
-
-  componentDidMount() {
-    fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-    .then (response => response.json())
-    .then (json => this.setState({ series: json }));
-  }
 
   render() {
     return (
@@ -23,7 +11,7 @@ class App extends React.Component {
             <h1 className="App-title">TV Series List</h1>
         </header>
         <Intro message="Here you can find all of your most loved series." />
-        The length of series array - {this.state.series.length}
+        <Series />
       </div>
     );
   }
