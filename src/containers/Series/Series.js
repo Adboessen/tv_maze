1740ef035236f7dbd816fa77b7626c1a4d7ee225
Andrew B <andrewboessen@amdg.rockhursths.edu>
2022-01-12 11:15:35 -0600

    Step 10

--- src/containers/Series/Series.js
+++ src/containers/Series/Series.js
@@ -1,27 +1,33 @@
-import React from 'react';
-import SeriesList from '../../components/SeriesList/SeriesList.js';
+import React from "react";
+import SeriesList from "../../components/SeriesList/SeriesList.js";
 class Series extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-          series: []
-        }
-    }
-    
-    componentDidMount() {
-        fetch('http://api.tvmaze.com/search/shows?q=Vikings')
-        .then (response => response.json())
-        .then (json => this.setState({ series: json }));
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      series: [],
+    };
+    this.onSeriesInputChange = this.onSeriesInputChange.bind(this);
+  }
 
-    render() {
-        return (
-            <div>
-                The length of series array - {this.state.series.length}
-                <SeriesList list={this.state.series} />
-            </div>
-        )
-    }
+  onSeriesInputChange(event) {
+    fetch(`http://api.tvmaze.com/search/shows?q=${event.target.value}`)
+      .then((response) => response.json())
+      .then((json) => this.setState({ series: json }));
+    console.log(event);
+    console.log(event.target.value);
+  }
+
+  render() {
+    return (
+      <div>
+        The length of series array - {this.state.series.length}
+        <div>
+          <input type="text" onChange={this.onSeriesInputChange} />
+        </div>
+        <SeriesList list={this.state.series} />
+      </div>
+    );
+  }
 }
 
 export default Series;
